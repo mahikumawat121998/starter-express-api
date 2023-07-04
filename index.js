765c74743e269b7684ce7b3d4ec497a5bd4eb494
Mahesh Kumawat <mahesh.kumawat@marktine.com>
2023-07-04 18:25:42 +0530

    main

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/users.js");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((e) => {
+    console.log("Mongoose connected successfully");
+  })
+  .catch((err) => console.log(err));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/mahilex", async (req, res) => {
+  try {
+    res.status(200).json({ name: "mahilex", age: 30, currency: "USD" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.listen(process.env.PORT||5000, () => {
+  console.log("Server is running on 5000 port");
+});
